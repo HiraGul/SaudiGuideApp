a60a474f8aec96e77363d6111c047edb0eba4294
infoshabbirkhan <infoshabbirkhan@gmail.com>
2023-05-21 14:32:50 +0500

    recomended

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:saudi_guide/Cubits/ORCCubit/ocrcubit_cubit.dart';
 import 'package:saudi_guide/Cubits/chat_bot_cubit/chat_bot_cubit.dart';
 import 'package:saudi_guide/Cubits/chat_list_cubit.dart';
 import 'package:saudi_guide/Cubits/my_recomendation_repo/my_recomendation_cubit.dart';
+import 'package:saudi_guide/Cubits/stable_disfussion_repo/text_to_image_cubit.dart';
 
 import 'Cubits/ScanCubit/scan_land_mark_cubit.dart';
 import 'Cubits/WeatherCubit/weather_forcast_cubit.dart';
@@ -51,7 +52,8 @@ class _MyAppState extends State<MyApp> {
         BlocProvider(
           create: (context) => ChatListCubit([]),
         ),
-        BlocProvider(create: (context) => TranslateListCubit())
+        BlocProvider(create: (context) => TranslateListCubit()),
+        BlocProvider(create: (context) => TextToImageCubit())
       ],
       child: MaterialApp(
           debugShowCheckedModeBanner: false,
